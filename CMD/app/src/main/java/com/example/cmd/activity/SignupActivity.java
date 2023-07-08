a31bb848f16e12b7e2ed56e8b0aeaf9ccdc0ef09
parkUiery <parkuiery@gmail.com>
2023-07-08 10:18:38 +0900

    🌱 :: 회원 가입 추가

---
+++ CMD/app/src/main/java/com/example/cmd/activity/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.cmd.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cmd.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
