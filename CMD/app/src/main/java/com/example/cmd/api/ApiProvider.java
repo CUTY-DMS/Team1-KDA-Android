72320491f5271659ed0525875cff9ecbccdb08a2
parkUiery <parkuiery@gmail.com>
2023-07-08 22:23:04 +0900

    🌱 :: BASE_URL 변경

--- CMD/app/src/main/java/com/example/cmd/api/ApiProvider.java
+++ CMD/app/src/main/java/com/example/cmd/api/ApiProvider.java
@@ -1,12 +1,13 @@
 package com.example.cmd.api;
 
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiProvider {
 
     private static Retrofit retrofit;
-    private static String BASE_URL = "";
+    private static String BASE_URL = "http://192.168.1.8:8080/";
 
     public static Retrofit getInstance() {
         if(retrofit == null){
