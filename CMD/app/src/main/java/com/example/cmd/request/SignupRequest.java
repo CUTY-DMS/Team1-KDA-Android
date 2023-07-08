4c7dd90a732ff1048029173ba87a76e6ec8438ea
parkUiery <parkuiery@gmail.com>
2023-07-08 22:24:18 +0900

    🌱 :: 회원 가입 요청 추가

--- CMD/app/src/main/java/com/example/cmd/request/SignupRequest.java
+++ CMD/app/src/main/java/com/example/cmd/request/SignupRequest.java
@@ -1,12 +1,27 @@
 package com.example.cmd.request;
 
+import android.util.Log;
+
 public class SignupRequest {
+    private String username;
     private String email;
     private String password;
 
-
-    public SignupRequest(String email, String password){
+    public SignupRequest(String username, String email, String password){
+        this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    public String getUsername() {
+        return  username;
+    }
+
+    public String getEmail() {
+        return  email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
