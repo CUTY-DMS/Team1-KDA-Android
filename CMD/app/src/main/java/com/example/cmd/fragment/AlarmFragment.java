e44441d498303681b425d6944e9d82e2ca3ec01c
parkUiery <parkuiery@gmail.com>
2023-07-08 14:50:01 +0900

    🌧 :: 프래그먼트 코드 수정

--- CMD/app/src/main/java/com/example/cmd/fragment/AlarmFragment.java
+++ CMD/app/src/main/java/com/example/cmd/fragment/AlarmFragment.java
@@ -9,36 +9,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.cmd.R;
+import com.example.cmd.databinding.FragmentAlarmBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AlarmFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AlarmFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    FragmentAlarmBinding binding;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public AlarmFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AlarmFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AlarmFragment newInstance(String param1, String param2) {
         AlarmFragment fragment = new AlarmFragment();
         Bundle args = new Bundle();
@@ -51,16 +33,20 @@ public class AlarmFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false);
+        binding = FragmentAlarmBinding.inflate(inflater);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        binding = null;
     }
 }
--- CMD/app/src/main/java/com/example/cmd/fragment/ScheduleFragment.java
+++ CMD/app/src/main/java/com/example/cmd/fragment/ScheduleFragment.java
@@ -9,18 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.cmd.R;
+import com.example.cmd.databinding.FragmentScheduleBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ScheduleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ScheduleFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FragmentScheduleBinding binding;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -30,20 +24,11 @@ public class ScheduleFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScheduleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ScheduleFragment newInstance(String param1, String param2) {
         ScheduleFragment fragment = new ScheduleFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +36,13 @@ public class ScheduleFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        binding = FragmentScheduleBinding.inflate(inflater);
+        return binding.getRoot();
     }
 }
