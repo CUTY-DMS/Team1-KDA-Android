eaaa31c2588c43d8e7ba2b3dcf38eda880b89532
parkUiery <parkuiery@gmail.com>
2023-07-11 22:44:46 +0900

    🌱 :: 내 정보 가져오기

--- CMD/app/src/main/java/com/example/cmd/fragment/ProfileFragment.java
+++ CMD/app/src/main/java/com/example/cmd/fragment/ProfileFragment.java
@@ -2,14 +2,22 @@ package com.example.cmd.fragment;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.example.cmd.R;
+import com.example.cmd.api.ApiProvider;
+import com.example.cmd.api.SeverApi;
 import com.example.cmd.databinding.FragmentProfileBinding;
+import com.example.cmd.response.MypageResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class ProfileFragment extends Fragment {
@@ -18,11 +26,9 @@ public class ProfileFragment extends Fragment {
     FragmentProfileBinding binding;
 
     public ProfileFragment() {
-        // Required empty public constructor
     }
 
 
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -41,6 +47,44 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         binding = FragmentProfileBinding.inflate(inflater);
+
+
+        bringInfo();
+
+
         return binding.getRoot();
+    }
+
+    private void bringInfo() {
+        TextView email = binding.textViewProfileEmail;
+        TextView name = binding.textViewProfileUserName;
+        TextView classIdNumber = binding.textViewProfileUserClassNumber;
+        TextView birth = binding.textViewProfileUserBirth;
+        TextView majorField = binding.textViewProfileUserMajor;
+        TextView clubName = binding.textViewProfileUserClub;
+
+        SeverApi severApi = ApiProvider.getInstance().create(SeverApi.class);
+
+        severApi.myPage().enqueue(new Callback<MypageResponse>() {
+            @Override
+            public void onResponse(Call<MypageResponse> call, Response<MypageResponse> response) {
+                if(response.isSuccessful()){
+                    if(response.body() != null){
+                        email.setText(response.body().getEmail());
+                        name.setText(response.body().getName());
+                        classIdNumber.setText(response.body().getClassIdNumber().toString());
+                        birth.setText(response.body().getBirth().toString());
+                        majorField.setText(response.body().getMajorField());
+                        clubName.setText(response.body().getClubName());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MypageResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
