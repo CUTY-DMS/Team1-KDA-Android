6c43f4ac412942fd0f1953d51b8b3d5eaffa599c
parkUiery <parkuiery@gmail.com>
2023-07-11 22:44:05 +0900

    🌱 :: 내 정보 폐이지 api 통신

--- CMD/app/src/main/java/com/example/cmd/api/SeverApi.java
+++ CMD/app/src/main/java/com/example/cmd/api/SeverApi.java
@@ -1,22 +1,39 @@
 package com.example.cmd.api;
 
 
+import com.example.cmd.request.ChangeMyInfoRequest;
 import com.example.cmd.request.LoginRequest;
 import com.example.cmd.request.SignupRequest;
+import com.example.cmd.response.MypageResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface SeverApi {
 
-    @POST("/users/signup")
+    @POST("/user/signup")
     Call<Void> signup (
             @Body SignupRequest signupRequest
     );
 
-    @POST("/users/login")
+    @POST("/user/login")
     Call<Void> login (
             @Body LoginRequest loginRequest
+    );
+
+    @GET("/user/myPage")
+    Call<MypageResponse> myPage (
+
+    );
+
+    @PATCH("/user/motifyUserInfo")
+    Call<Void> changeInfo (
+            @Header("AccessToken") String token
+             ChangeMyInfoRequest chan
     );
 }
