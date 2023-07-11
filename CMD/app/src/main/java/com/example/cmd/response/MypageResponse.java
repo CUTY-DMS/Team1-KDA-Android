b523380c068f32401588539ae87c3f73628581b7
parkUiery <parkuiery@gmail.com>
2023-07-11 22:45:35 +0900

    🌱 :: 내 정보 api 응답

---
+++ CMD/app/src/main/java/com/example/cmd/response/MypageResponse.java
@@ -0,0 +1,35 @@
+package com.example.cmd.response;
+
+public class MypageResponse {
+
+    private String name;
+    private String email;
+    private Long classIdNumber;
+    private Long birth;
+    private String majorField;
+    private String clubName;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getClassIdNumber() {
+        return classIdNumber;
+    }
+
+    public Long getBirth() {
+        return birth;
+    }
+
+    public String getMajorField() {
+        return majorField;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+}
