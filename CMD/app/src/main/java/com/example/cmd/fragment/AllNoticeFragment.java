fc54a55f057f38d99fdefb4d0278795ef9de3ae0
parkUiery <parkuiery@gmail.com>
2023-07-13 22:37:25 +0900

    🌧 :: 전체 공지 추가

--- CMD/app/src/main/java/com/example/cmd/fragment/AllNoticeFragment.java
+++ CMD/app/src/main/java/com/example/cmd/fragment/AllNoticeFragment.java
@@ -9,58 +9,32 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.cmd.R;
+import com.example.cmd.databinding.FragmentAllNoticeBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AllNoticeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AllNoticeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AllNoticeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AllNoticeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AllNoticeFragment newInstance(String param1, String param2) {
-        AllNoticeFragment fragment = new AllNoticeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    FragmentAllNoticeBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_notice, container, false);
+        binding = FragmentAllNoticeBinding.inflate(inflater);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        binding = null;
     }
 }
