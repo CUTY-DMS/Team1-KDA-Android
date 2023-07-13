6065746d1cab787da900a95bc3413d9627a7b648
parkUiery <parkuiery@gmail.com>
2023-07-13 22:36:03 +0900

    🌱 :: 알람 탑 레이아웃 어뎁터 추가

--- CMD/app/src/main/java/com/example/cmd/adapter/AlarmAdapter.java
+++ CMD/app/src/main/java/com/example/cmd/adapter/AlarmAdapter.java
@@ -1,2 +1,34 @@
-package com.example.cmd.adapter;public class AlarmAdapter {
+package com.example.cmd.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlarmAdapter extends FragmentPagerAdapter, RecyclerView.Adapter<AlarmAdapter.ViewHolder> {
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+
+    public AlarmAdapter(@NonNull FragmentManager fm, int behavior){
+        super(fm,behavior);
+    }
+
+    public void addFragment(Fragment fragment){
+        fragmentList.add(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
 }
