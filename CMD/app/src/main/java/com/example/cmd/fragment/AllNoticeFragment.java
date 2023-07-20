77838ce383fada08fb2368f371d23bd9d711a15f
parkUiery <parkuiery@gmail.com>
2023-07-20 20:51:30 +0900

    ♻️ :: 리사이클러뷰 코드 수정

--- CMD/app/src/main/java/com/example/cmd/fragment/AllNoticeFragment.java
+++ CMD/app/src/main/java/com/example/cmd/fragment/AllNoticeFragment.java
@@ -3,17 +3,26 @@ package com.example.cmd.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.cmd.R;
 import com.example.cmd.activity.LoginActivity;
+import com.example.cmd.adapter.AllNoticeAdapter;
 import com.example.cmd.api.ApiProvider;
 import com.example.cmd.api.SeverApi;
 import com.example.cmd.databinding.FragmentAllNoticeBinding;
+import com.example.cmd.response.AllNoticeResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,6 +33,12 @@ public class AllNoticeFragment extends Fragment {
 
 
     FragmentAllNoticeBinding binding;
+
+    private RecyclerView recyclerView;
+    private AllNoticeAdapter adapter;
+    private LinearLayoutManager linearLayoutManager;
+
+    List<AllNoticeResponse> allNoticeResponseList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,16 +51,29 @@ public class AllNoticeFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentAllNoticeBinding.inflate(inflater);
 
+        allNoticeResponseList = new ArrayList<>();
+
+        recyclerView = binding.recyclerViewAllNotice;
+
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new AllNoticeAdapter(allNoticeResponseList);
+
+        recyclerView.setAdapter(adapter);
+
         SeverApi severApi = ApiProvider.getInstance().create(SeverApi.class);
 
-        severApi.allNotice("Bearer " + LoginActivity.accessToken).enqueue(new Callback<Void>() {
+        severApi.allNotice(LoginActivity.accessToken).enqueue(new Callback<List<AllNoticeResponse>>() {
             @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-
+            public void onResponse(Call<List<AllNoticeResponse>> call, Response<List<AllNoticeResponse>> response) {
+                allNoticeResponseList.addAll(response.body());
+                adapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onFailure(Call<Void> call, Throwable t) {
+            public void onFailure(Call<List<AllNoticeResponse>> call, Throwable t) {
 
             }
         });
