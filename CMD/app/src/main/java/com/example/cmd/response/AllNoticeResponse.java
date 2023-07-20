cd7c3f5fca0c593293ce54a77ae1c0557fcf8f77
parkUiery <parkuiery@gmail.com>
2023-07-20 20:52:06 +0900

    🌱 :: 전체 공지 응답

---
+++ CMD/app/src/main/java/com/example/cmd/response/AllNoticeResponse.java
@@ -0,0 +1,16 @@
+package com.example.cmd.response;
+
+import java.util.List;
+
+public class AllNoticeResponse {
+    private String title;
+    private String dateTime;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDateTime() {
+        return  dateTime;
+    }
+}
