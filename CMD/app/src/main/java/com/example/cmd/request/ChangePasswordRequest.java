7e2cd82bba711167c98581227dfb39a6b432a09a
parkUiery <parkuiery@gmail.com>
2023-07-21 22:37:31 +0900

    🌱 :: 비밀번호 변경

---
+++ CMD/app/src/main/java/com/example/cmd/request/ChangePasswordRequest.java
@@ -0,0 +1,28 @@
+package com.example.cmd.request;
+
+public class ChangePasswordRequest {
+
+    private String oldPassword; //예전 비밀번호
+    private String newPassword; //비밀번호 입력
+    private String reNewPassword; //비밀번호 확인
+
+
+
+    public ChangePasswordRequest (String oldPassword, String newPassword, String reNewPassword){
+        this.oldPassword = oldPassword;
+        this.newPassword = newPassword;
+        this.reNewPassword= reNewPassword;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public String getReNewPassword() {
+        return reNewPassword;
+    }
+}
