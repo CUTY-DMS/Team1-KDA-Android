f8e70bd0ee9652794c233626aefd937b9902ab22
parkUiery <parkuiery@gmail.com>
2023-07-21 22:36:58 +0900

    🌧 :: 비밀번호 변경 api 추가

--- CMD/app/src/main/java/com/example/cmd/api/SeverApi.java
+++ CMD/app/src/main/java/com/example/cmd/api/SeverApi.java
@@ -2,6 +2,7 @@ package com.example.cmd.api;
 
 
 import com.example.cmd.request.ChangeMyInfoRequest;
+import com.example.cmd.request.ChangePasswordRequest;
 import com.example.cmd.request.LoginRequest;
 import com.example.cmd.request.SignupRequest;
 import com.example.cmd.response.AllNoticeResponse;
@@ -21,28 +22,35 @@ import retrofit2.http.Path;
 
 public interface SeverApi {
 
-    @POST("/user/signup")
+    @POST("/user/signup")  //회원 가입
     Call<Void> signup (
             @Body SignupRequest signupRequest
     );
 
-    @POST("/user/login")
+    @POST("/user/login")   //로그인
     Call<LoginResponse> login (
             @Body LoginRequest loginRequest
     );
 
-    @GET("/user/myPage")
+    @GET("/user/myPage")   //마이페이지
     Call<MypageResponse> myPage (
 
     );
 
-    @PATCH("/user/motifyUserInfo")
+    @PATCH("/user/motifyUserInfo")  //내 정보 변경
     Call<Void> changeInfo (
             @Header("Authorization") String accessToken,
             @Body ChangeMyInfoRequest changeMyInfoRequest
     );
 
-    @GET("/user/allNoti")
+    @PATCH("/admin/password/change")  //비밀번호 변경
+    Call<Void> changePassword (
+            @Header("Authorization") String accessToken,
+            @Body ChangePasswordRequest changePasswordRequest
+    );
+
+
+    @GET("/user/allNoti")  //전체 공지 리스트 확인
     Call<List<AllNoticeResponse>> allNotice (
             @Header("Authorization") String Token
     );
