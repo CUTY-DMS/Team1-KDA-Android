bf726cefbf7d00501aa2e98a5b792d0205a448dd
parkUiery <parkuiery@gmail.com>
2023-07-22 21:48:00 +0900

    🌱 :: 우리 반 공지

---
+++ CMD/app/src/main/java/com/example/cmd/response/WeClassResponse.java
@@ -0,0 +1,15 @@
+package com.example.cmd.response;
+
+public class WeClassResponse {
+
+    private String title;
+    private String dateTime;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+}
