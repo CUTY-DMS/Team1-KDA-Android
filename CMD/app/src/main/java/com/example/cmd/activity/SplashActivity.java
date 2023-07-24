27bb614b3411e6601a6f8030e561eaaa4764ddec
parkUiery <parkuiery@gmail.com>
2023-07-24 22:18:40 +0900

    🌧 :: 스플래시 애니메이션

--- CMD/app/src/main/java/com/example/cmd/activity/SplashActivity.java
+++ CMD/app/src/main/java/com/example/cmd/activity/SplashActivity.java
@@ -20,31 +20,54 @@ public class SplashActivity extends AppCompatActivity {
     private ScalableTextView textViewDescription2;
     private ScalableTextView textViewDescription3;
 
+    private ScalableTextView textViewK;
+    private ScalableTextView textViewD;
+    private ScalableTextView textViewA;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        textViewKDA = findViewById(R.id.textview_KDA);
         textViewDescription1 = findViewById(R.id.textview_KDA_description1);
         textViewDescription2 = findViewById(R.id.textview_KDA_description2);
         textViewDescription3 = findViewById(R.id.textview_KDA_description3);
+
+        textViewK = findViewById(R.id.textview_K);
+        textViewD = findViewById(R.id.textview_D);
+        textViewA = findViewById(R.id.textview_A);
+
+        textViewK.bringToFront();
+        textViewD.bringToFront();
+        textViewA.bringToFront();
+
+
 
         // KDA 글씨를 크게 보이도록 애니메이션 설정
         //Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
 
         //textViewKDA.startAnimation(fadeInAnimation);
 
-        textViewKDA.animateLetterSpacing(1.4f,1000);
+        //textViewKDA.animateLetterSpacing(1.4f,1000);
+
+        Animation slideUpAnimation_k = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.slide_k);
+        Animation slideUpAnimation_d = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.slide_d);
+        Animation slideUpAnimation_a = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.slide_a);
+
+
+        textViewK.startAnimation(slideUpAnimation_k);
+        textViewD.startAnimation(slideUpAnimation_d);
+        textViewA.startAnimation(slideUpAnimation_a);
+
 
         // 일정 시간 후에 KDA 글씨를 보여주는 애니메이션 완료 후, 나머지 텍스트 표시
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                textViewKDA.setVisibility(View.VISIBLE);
 
                 // "Korea Daedeok Assistance" 텍스트를 보여주도록 애니메이션 설정
                 Animation slideUpAnimation = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.slide);
+
                 textViewDescription1.startAnimation(slideUpAnimation);
                 textViewDescription2.startAnimation(slideUpAnimation);
                 textViewDescription3.startAnimation(slideUpAnimation);
@@ -68,6 +91,6 @@ public class SplashActivity extends AppCompatActivity {
                     }
                 });
             }
-        }, 1000); // 1초 후에 텍스트 표시 애니메이션 실행
+        }, 800); // 1초 후에 텍스트 표시 애니메이션 실행
     }
 }
