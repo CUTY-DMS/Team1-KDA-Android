783030fe2d62cfa564233498a8089546e84610f9
parkUiery <parkuiery@gmail.com>
2023-07-25 22:49:05 +0900

    🌱 :: 다이어로그 추가

--- CMD/app/src/main/java/com/example/cmd/etc/AllNoticeDialog.java
+++ CMD/app/src/main/java/com/example/cmd/etc/AllNoticeDialog.java
@@ -1,2 +1,44 @@
-package com.example.cmd.etc;public class AllNoticeDialog {
+package com.example.cmd.etc;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.cmd.R;
+import com.example.cmd.databinding.ActivityAllNoticeDialogBinding;
+
+public class AllNoticeDialog extends Dialog {
+
+    private TextView title;
+    private TextView date;
+    private TextView detail;
+
+    ActivityAllNoticeDialogBinding binding;
+
+
+    public AllNoticeDialog(@NonNull Context context, String themeResId) {
+        super(context);
+
+        binding = ActivityAllNoticeDialogBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        title = binding.textviewAllDialogUserTitle;
+        date = binding.textviewAllDialogUserDate;
+        detail = binding.textviewAllDialogUserDetail;
+
+        detail.setText(themeResId);
+
+        binding.buttonAllDialogShut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
 }
