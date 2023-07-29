b29fd62cde175514af9ec97cc3c5684ba93f2fcf
parkUiery <parkuiery@gmail.com>
2023-07-29 20:48:30 +0900

    🌧 :: 전체 공지 알림 이름 추가 api

--- CMD/app/src/main/java/com/example/cmd/response/AllNoticeResponse.java
+++ CMD/app/src/main/java/com/example/cmd/response/AllNoticeResponse.java
@@ -5,6 +5,7 @@ package com.example.cmd.response;
 public class AllNoticeResponse {
     private String title;
     private String dateTime;
+    private String name;
 
     public String getTitle() {
         return title;
@@ -12,5 +13,9 @@ public class AllNoticeResponse {
 
     public String getDateTime() {
         return  dateTime;
+    }
+
+    public String getName() {
+        return name;
     }
 }
