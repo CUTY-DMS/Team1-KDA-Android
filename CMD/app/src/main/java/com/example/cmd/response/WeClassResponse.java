3649d96f74ddf3111853b7c2440c0006ed9b9a6a
parkUiery <parkuiery@gmail.com>
2023-07-31 20:38:57 +0900

    🌧 :: 이름 추가

--- CMD/app/src/main/java/com/example/cmd/response/WeClassResponse.java
+++ CMD/app/src/main/java/com/example/cmd/response/WeClassResponse.java
@@ -5,11 +5,17 @@ public class WeClassResponse {
     private String title;
     private String dateTime;
 
+    private String name;
+
     public String getTitle() {
         return title;
     }
 
     public String getDateTime() {
         return dateTime;
+    }
+
+    public String getName() {
+        return name;
     }
 }
