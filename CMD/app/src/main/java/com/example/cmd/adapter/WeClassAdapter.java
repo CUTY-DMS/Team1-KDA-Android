704f79c253e0b34c21593776aa87e04799ae8c5d
parkUiery <parkuiery@gmail.com>
2023-07-31 20:37:58 +0900

    🌧 :: 시간 파싱, 제목 추가

--- CMD/app/src/main/java/com/example/cmd/adapter/WeClassAdapter.java
+++ CMD/app/src/main/java/com/example/cmd/adapter/WeClassAdapter.java
@@ -12,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.cmd.R;
 import com.example.cmd.response.WeClassResponse;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class WeClassAdapter extends RecyclerView.Adapter<WeClassAdapter.ItemViewHolder> {
@@ -23,12 +25,14 @@ public class WeClassAdapter extends RecyclerView.Adapter<WeClassAdapter.ItemView
 
         public TextView name;
         public TextView date;
+        public TextView title;
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
 
             name = itemView.findViewById(R.id.textview_weClass_user);
             date = itemView.findViewById(R.id.textview_weClass_date);
+            title = itemView.findViewById(R.id.textview_weClass_alarm);
         }
     }
 
@@ -52,8 +56,20 @@ public class WeClassAdapter extends RecyclerView.Adapter<WeClassAdapter.ItemView
         Log.d("TEST","우리 반 알림/ name "+holder.name+"/date "+holder.date);
         Log.d("TEST","c/"+list.get(position).getTitle());
 
+
+
+        String inputDateString = list.get(position).getDateTime();
+
+
+        DateTimeFormatter input = DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm:ss");
+        DateTimeFormatter output = DateTimeFormatter.ofPattern("yy.MM.dd");
+
+        LocalDateTime dateTime = LocalDateTime.parse(inputDateString,input);
+        String outDate = dateTime.format(DateTimeFormatter.ofPattern("yy.MM.dd"));
+
         holder.name.setText(list.get(position).getTitle());
-        holder.date.setText(list.get(position).getDateTime());
+        holder.title.setText(list.get(position).getTitle());
+        holder.date.setText(outDate);
 
 
     }
