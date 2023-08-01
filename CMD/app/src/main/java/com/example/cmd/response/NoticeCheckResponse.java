3ac90d2d916b46ee219f1a98951d49467f835821
parkUiery <parkuiery@gmail.com>
2023-08-01 19:52:06 +0900

    🌱 :: 공지 상세보기 api

---
+++ CMD/app/src/main/java/com/example/cmd/response/NoticeCheckResponse.java
@@ -0,0 +1,24 @@
+package com.example.cmd.response;
+
+public class NoticeCheckResponse {
+    private String title;
+    private String contents;
+    private String dateTime;
+    private String name;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
