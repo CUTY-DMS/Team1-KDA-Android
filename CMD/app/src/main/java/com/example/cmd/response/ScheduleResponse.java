9ed4b4c01f7d14f3e6b4e186b77e1cfe934155b1
parkUiery <parkuiery@gmail.com>
2023-08-03 23:48:39 +0900

    🌱 :: 시간표 api 추가

--- CMD/app/src/main/java/com/example/cmd/response/ScheduleResponse.java
+++ CMD/app/src/main/java/com/example/cmd/response/ScheduleResponse.java
@@ -1,2 +1,26 @@
-package com.example.cmd.response;public class ScheduleResponse {
+package com.example.cmd.response;
+
+
+public class ScheduleResponse {
+
+    private String GRADE;
+    private String CLASS_NM;
+    private String PERIO;
+    private String ITRT_CNTNT;
+
+    public String getGRADE() {
+        return GRADE;
+    }
+
+    public String getCLASS_NM() {
+        return CLASS_NM;
+    }
+
+    public String getPERIO() {
+        return PERIO;
+    }
+
+    public String getITRT_CNTNT() {
+        return ITRT_CNTNT;
+    }
 }
