e888749d67c957a2e8a76945dedada654440f71e
parkUiery <parkuiery@gmail.com>
2023-08-04 17:48:15 +0900

    🌧 :: 시간표 api 파싱

--- CMD/app/src/main/java/com/example/cmd/response/ScheduleResponse.java
+++ CMD/app/src/main/java/com/example/cmd/response/ScheduleResponse.java
@@ -1,26 +1,83 @@
 package com.example.cmd.response;
 
 
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class ScheduleResponse {
 
-    private String GRADE;
-    private String CLASS_NM;
-    private String PERIO;
-    private String ITRT_CNTNT;
+//    @SerializedName("GRADE")
+//    @Expose
+//    private String grade;
+//    @SerializedName("CLASS_NM")
+//    @Expose
+//    private String classNm;
+//    @SerializedName("PERIO")
+//    @Expose
+//    private String perio;
+//    @SerializedName("ITRT_CNTNT")
+//    @Expose
+//    private String itrtCntnt;
 
-    public String getGRADE() {
-        return GRADE;
+    //---------------------------
+    @SerializedName("hisTimetable")
+    @Expose
+    private List<ScheduleHisTimetable> hisTimetable;
+
+    public List<ScheduleHisTimetable> getHisTimetable() {
+        return hisTimetable;
     }
 
-    public String getCLASS_NM() {
-        return CLASS_NM;
+    @Override
+    public String toString() {
+        return "scheduleItems: " + hisTimetable;
     }
 
-    public String getPERIO() {
-        return PERIO;
-    }
-
-    public String getITRT_CNTNT() {
-        return ITRT_CNTNT;
-    }
+    //    public String getGrade() {
+//        return grade;
+//    }
+//
+//    public void setGrade(String grade) {
+//        this.grade = grade;
+//    }
+//
+//    public String getClassNm() {
+//        return classNm;
+//    }
+//
+//    public void setClassNm(String classNm) {
+//        this.classNm = classNm;
+//    }
+//
+//    public String getPerio() {
+//        return perio;
+//    }
+//
+//    public void setPerio(String perio) {
+//        this.perio = perio;
+//    }
+//
+//    public String getItrtCntnt() {
+//        return itrtCntnt;
+//    }
+//
+//    public void setItrtCntnt(String itrtCntnt) {
+//        this.itrtCntnt = itrtCntnt;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "ScheduleResponse{" +
+//                "grade='" + grade + '\'' +
+//                ", classNm='" + classNm + '\'' +
+//                ", perio='" + perio + '\'' +
+//                ", itrtCntnt=" + itrtCntnt +
+//                '}';
+//
+//
+//    }
 }
