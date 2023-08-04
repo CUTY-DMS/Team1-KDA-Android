22e2118f825579db47ec20c974534ba0c3bbe278
parkUiery <parkuiery@gmail.com>
2023-08-04 17:46:54 +0900

    🌱 :: 시간표 api 파싱

--- CMD/app/src/main/java/com/example/cmd/response/ScheduleItemResponse.java
+++ CMD/app/src/main/java/com/example/cmd/response/ScheduleItemResponse.java
@@ -1,2 +1,24 @@
-package com.example.cmd.response;public class ScheduleItemResponse {
+package com.example.cmd.response;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ScheduleItemResponse {
+    @SerializedName("ITRT_CNTNT")
+    @Expose
+    private String itrtCntnt;
+
+    public String getItrtCntnt() {
+        return itrtCntnt;
+    }
+
+
+    @Override
+    public String toString() {
+        return "시간표: " + itrtCntnt;
+    }
 }
