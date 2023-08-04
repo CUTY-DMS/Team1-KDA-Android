43258fb8b0bcf2d32e8e2097ada4a3980c8f3959
parkUiery <parkuiery@gmail.com>
2023-08-04 17:46:02 +0900

    🌧 :: 시간표 api 파싱

--- CMD/app/src/main/java/com/example/cmd/fragment/schedule/MonFragment.java
+++ CMD/app/src/main/java/com/example/cmd/fragment/schedule/MonFragment.java
@@ -3,64 +3,169 @@ package com.example.cmd.fragment.schedule;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.cmd.R;
+import com.example.cmd.adapter.MonAdapter;
+import com.example.cmd.api.SeverApi;
+import com.example.cmd.databinding.FragmentMonBinding;
+import com.example.cmd.response.ScheduleHisTimetable;
+import com.example.cmd.response.ScheduleItemResponse;
+import com.example.cmd.response.ScheduleResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MonFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MonFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FragmentMonBinding binding;
+    private static final String BASE_URL = "https://open.neis.go.kr/hub/";
+    private static final String KEY = "&KEY=513aa74951a64b0793c9a0519e3e4bde";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static String grade;
+    private static String classNm;
+    private static String date;
+
+    List<ScheduleItemResponse> scheduleItemResponseList;
+    MonAdapter monAdapter;
+    RecyclerView recyclerView;
 
     public MonFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MonFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MonFragment newInstance(String param1, String param2) {
-        MonFragment fragment = new MonFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mon, container, false);
+        binding = FragmentMonBinding.inflate(inflater);
+
+        grade = "1";
+        classNm = "3";
+        date = "20230703";
+
+        recyclerView = binding.recyclerViewMon;
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        scheduleItemResponseList = new ArrayList<>();
+        monAdapter = new MonAdapter(scheduleItemResponseList);
+        Log.d("TEST","시간표 리스트/" + scheduleItemResponseList);
+        recyclerView.setAdapter(monAdapter);
+
+
+
+
+        // HttpLoggingInterceptor 인스턴스 생성
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+// OkHttpClient에 인터셉터 추가
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                // 다른 필요한 인터셉터들도 추가할 수 있음
+                .build();
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        SeverApi severApi = retrofit.create(SeverApi.class);
+
+        Call<ScheduleResponse> call = severApi.scheduleList(grade,classNm,date,KEY);
+
+        call.enqueue(new Callback <ScheduleResponse>() {
+            @Override
+            public void onResponse(Call<ScheduleResponse> call, Response<ScheduleResponse> response) {
+                if (response.isSuccessful()) {
+                    //List<ScheduleResponse> scheduleList = response.body();
+
+                    // JSON 배열을 JSON 객체로 래핑
+                    JsonObject jsonObject = new JsonObject();
+                    JsonArray jsonArray = new JsonArray();
+//                    for (ScheduleResponse schedule : scheduleList) {
+//                        JsonObject scheduleObject = new JsonObject();
+//                        scheduleObject.addProperty("GRADE", schedule.getGrade());
+//                        scheduleObject.addProperty("CLASS_NM", schedule.getClassNm());
+//                        scheduleObject.addProperty("PERIO", schedule.getPerio());
+//                        scheduleObject.addProperty("ITRT_CNTNT", schedule.getItrtCntnt());
+//                        jsonArray.add(scheduleObject);
+//                    }
+
+                    Log.d("TEST","응답/"+response.body());
+                    ScheduleResponse scheduleResponse = response.body();
+
+                    if (scheduleResponse != null) {
+                        List<ScheduleHisTimetable> scheduleItems = scheduleResponse.getHisTimetable();
+                        //List<ScheduleItemResponse> itemResponseList = response.body();
+                        for (ScheduleHisTimetable item : scheduleItems) {
+                            JsonObject scheduleObject = new JsonObject();
+                            //scheduleObject.addProperty("ITRT_CNTNT", item.getScheduleItems().toString());
+                            Log.d("TEST","d/"+response.body().getHisTimetable());
+                            Log.d("TEST","아이템/"+item.getScheduleItems());
+                            //String itrtCntnt = item.getItrtCntnt();
+                            Log.d("TEST","f/"+scheduleItems);
+                            //Log.d("TEST", "ITRT_CNTNT: " + itrtCntnt);
+                            jsonArray.add(scheduleObject);
+                        }
+                        //scheduleItemResponseList.addAll(response.body());
+                        monAdapter.notifyDataSetChanged();
+                    }
+                    jsonObject.add("items", jsonArray);
+
+                    // 결과 출력
+                    Gson gson = new Gson();
+                    String jsonObjectString = gson.toJson(jsonObject);
+                    //Log.d("TEST","시단표/"+response.body().getPerio());
+                    Log.d("TEST","시간표 url/ "+ response.raw().request().url().url());
+                    Log.d("TEST", "시간표 JSON: " + jsonObjectString);
+                } else {
+                    // API 호출 실패 처리
+                    Log.d("TEST", "API 호출 실패 코드: " + response.code());
+                    Log.d("TEST", "연결 주소 확인: " + response.raw().request().url().url());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ScheduleResponse> call, Throwable t) {
+                Log.d("TEST", "통신 실패: " + t.getMessage());
+                Log.d("TEST", "실패: " + httpClient);
+            }
+        });
+
+
+
+
+        return binding.getRoot();
     }
 }
