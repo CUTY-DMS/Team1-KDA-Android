eb12346c076fa16d07e1e007f3d70aedb5c5b9c5
parkUiery <parkuiery@gmail.com>
2023-08-06 08:17:55 +0900

    🌧 :: 달력 일정 api

--- CMD/app/src/main/java/com/example/cmd/response/CalendarResponse.java
+++ CMD/app/src/main/java/com/example/cmd/response/CalendarResponse.java
@@ -3,13 +3,23 @@ package com.example.cmd.response;
 public class CalendarResponse {
 
     private String title;
-    private Long date;
+    private Long year;
+    private Long month;
+    private Long day;
 
     public String getTitle() {
         return title;
     }
 
-    public Long getDate() {
-        return date;
+    public Long getYear() {
+        return year;
+    }
+
+    public Long getMonth() {
+        return month;
+    }
+
+    public Long getDay() {
+        return day;
     }
 }
