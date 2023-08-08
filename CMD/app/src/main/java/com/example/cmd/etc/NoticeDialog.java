076241a3e3532f438f4ee2a14671d707028e4618
parkUiery <parkuiery@gmail.com>
2023-08-08 12:37:18 +0900

    ♻ :: 코드 정렬

--- CMD/app/src/main/java/com/example/cmd/etc/NoticeDialog.java
+++ CMD/app/src/main/java/com/example/cmd/etc/NoticeDialog.java
@@ -2,29 +2,22 @@ package com.example.cmd.etc;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.util.Log;
 import android.view.View;
-
 import androidx.annotation.NonNull;
-
 import com.example.cmd.activity.LoginActivity;
 import com.example.cmd.api.ApiProvider;
 import com.example.cmd.api.SeverApi;
 import com.example.cmd.databinding.ActivityAllNoticeDialogBinding;
 import com.example.cmd.response.NoticeCheckResponse;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class NoticeDialog extends Dialog {
 
-
     ActivityAllNoticeDialogBinding binding;
-
 
     public NoticeDialog(@NonNull Context context, Long id) {
         super(context);
@@ -32,9 +25,7 @@ public class NoticeDialog extends Dialog {
         binding = ActivityAllNoticeDialogBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Log.d("TEST","id"+id);
         sever(id);
-
 
         binding.buttonAllDialogShut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,13 +41,11 @@ public class NoticeDialog extends Dialog {
         severApi.check(LoginActivity.accessToken, id).enqueue(new Callback<NoticeCheckResponse>() {
             @Override
             public void onResponse(Call<NoticeCheckResponse> call, Response<NoticeCheckResponse> response) {
-                if(response.isSuccessful()){
+                if (response.isSuccessful()) {
                     String inputDateString = response.body().getDateTime();
-
-
                     DateTimeFormatter input = DateTimeFormatter.ofPattern("yyyy-MM-ddHH:mm:ss");
 
-                    LocalDateTime dateTime = LocalDateTime.parse(inputDateString,input);
+                    LocalDateTime dateTime = LocalDateTime.parse(inputDateString, input);
                     String outDate = dateTime.format(DateTimeFormatter.ofPattern("yy.MM.dd"));
 
                     binding.textviewAllDialogUserTitle.setText(response.body().getTitle());
@@ -72,6 +61,5 @@ public class NoticeDialog extends Dialog {
             }
         });
     }
-
 
 }
