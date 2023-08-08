6d4b33611878d966b191a21f941b79280ab552da
parkUiery <parkuiery@gmail.com>
2023-08-08 12:36:06 +0900

    ♻ :: 코드 정렬

--- CMD/app/src/main/java/com/example/cmd/api/ApiProvider.java
+++ CMD/app/src/main/java/com/example/cmd/api/ApiProvider.java
@@ -7,10 +7,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiProvider {
 
     private static Retrofit retrofit;
-    private static String BASE_URL = "http://:8080";
+    private static String BASE_URL = "http://";
 
     public static Retrofit getInstance() {
-        if(retrofit == null){
+        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
--- CMD/app/src/main/java/com/example/cmd/api/SeverApi.java
+++ CMD/app/src/main/java/com/example/cmd/api/SeverApi.java
@@ -14,9 +14,7 @@ import com.example.cmd.response.NoticeCheckResponse;
 import com.example.cmd.response.ReissueResponse;
 import com.example.cmd.response.ScheduleResponse;
 import com.example.cmd.response.WeClassResponse;
-
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -28,50 +26,50 @@ import retrofit2.http.Query;
 
 public interface SeverApi {
 
-    @POST("/user/signup")  //회원 가입
+    @POST("/user/signup")//회원 가입
     Call<Void> signup (
             @Body SignupRequest signupRequest
     );
 
-    @POST("/user/login")   //로그인
+    @POST("/user/login")//로그인
     Call<LoginResponse> login (
             @Body LoginRequest loginRequest
     );
 
-    @GET("/user/myPage")   //마이페이지
+    @GET("/user/myPage")//마이페이지
     Call<MypageResponse> myPage (
             @Header("Authorization") String accessToken
     );
 
-    @PATCH("/user/infoChange")  //내 정보 변경
+    @PATCH("/user/infoChange")//내 정보 변경
     Call<Void> changeInfo (
             @Header("Authorization") String accessToken,
             @Body ChangeMyInfoRequest changeMyInfoRequest
     );
 
-    @PATCH("/user/password/change")  //비밀번호 변경
+    @PATCH("/user/password/change")//비밀번호 변경
     Call<Void> changePassword (
             @Header("Authorization") String accessToken,
             @Body ChangePasswordRequest changePasswordRequest
     );
 
-    @GET("/user/allNoti")  //전체 공지 리스트 확인
+    @GET("/user/allNoti")//전체 공지 리스트 확인
     Call<List<AllNoticeResponse>> allNotice (
             @Header("Authorization") String accessToken
     );
 
-    @GET("/user/classNoti")  //우리 반 공지 리스트 확인
+    @GET("/user/classNoti")//우리 반 공지 리스트 확인
     Call<List<WeClassResponse>> weClass (
             @Header("Authorization") String accessToken
     );
 
-    @GET("/noti/check/{notiId}")  //공지 상세 보기
+    @GET("/noti/check/{notiId}")//공지 상세 보기
     Call<NoticeCheckResponse> check (
             @Header("Authorization") String accessToken,
             @Path("notiId") long id
     );
 
-    @GET("hisTimetable?ATPT_OFCDC_SC_CODE=G10&SD_SCHUL_CODE=7430310&Type=json") //시간표
+    @GET("hisTimetable?ATPT_OFCDC_SC_CODE=G10&SD_SCHUL_CODE=7430310&Type=json")//시간표
     Call<ScheduleResponse> scheduleList (
             @Query("GRADE") String grade,
             @Query("CLASS_NM") String classNm,
@@ -79,19 +77,19 @@ public interface SeverApi {
             @Query("KEY") String key
     );
 
-    @GET("/user/schedule/{year}/{month}") //달력 일정 보기
+    @GET("/user/schedule/{year}/{month}")//달력 일정 보기
     Call<List<CalendarResponse>> calendar (
             @Header("Authorization") String accessToken,
             @Path("year") int year,
             @Path("month") int mont
     );
 
-    @GET("/user/gradeClass")  //학년 반 보기
+    @GET("/user/gradeClass")//학년 반 보기
     Call<GradeClassResponse> grade (
             @Header("Authorization") String accessToken
     );
 
-    @POST("/reissue")  //토큰 재발급
+    @POST("/reissue")//토큰 재발급
     Call<ReissueResponse> reissue (
             @Header("AUTHORIZATION_HEADER") String refreshToken
     );
